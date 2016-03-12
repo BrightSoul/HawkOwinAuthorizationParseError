e33520a5643e3b63233bd7c7e954de3f9f11d886
Moreno Gentili <brightsoul@tiscali.it>
2016-03-13 00:06:15 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HawkOwinAuthorizationParseError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+
+                Console.WriteLine("Server started. Make an unauthenticated GET request to {0}", baseAddress);
+                Console.WriteLine("Press ENTER to quit.");
+
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using HawkNet;
+using HawkNet.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HawkOwinAuthorizationParseError
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+            app.UseHawkAuthentication(new HawkAuthenticationOptions {
+                  Credentials = id => Task.FromResult(new HawkCredential { Id = id, Key = "aaa", User = "bob", Algorithm = "sha256"  }),
+                  IncludeServerAuthorization = true
+            });
+            
+            app.Run(async context =>
+            {
+                bool authenticated = context.Authentication != null && context.Authentication.User != null && context.Authentication.User.Identity.IsAuthenticated;
+                    await context.Response.WriteAsync(string.Format("You are authenticated: {0}", authenticated));
+            });
+        }
+    }
+}
